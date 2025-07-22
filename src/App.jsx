8084ce6806efe6e8a1a47c97eeb55573f397aac8
Dev-43 <deveshdolas9@gmail.com>
2025-07-22 15:40:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoyaltyCardPage from './components/LoyaltyCardPage';
+import './App.css';
+
+function App() {
+  return <LoyaltyCardPage />;
+}
+
+export default App;
